00a75478f4f87cbc172346f539e59b1c9354bebc
Dave M <dave.nerd@gmail.com>
2012-06-28 16:45:22 -0500

    updated template to fit current mold

--- template.pl
+++ template.pl
@@ -1,21 +1,37 @@
 #!/usr/bin/perl
 
+package foo;
+
 use strict;
 use warnings;
 
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-# Our main window: it holds everything
-my $window = Gtk3::Window->new;
-$window->set_title( 'template' );
-$window->signal_connect( destroy => sub { Gtk3->main_quit } );
+my $window;
 
-# This VBox will be handy to organize objects
-my $box = Gtk3::Box->new ( 'vertical', 5 );
-$window->add( $box );
-
-# Tell everything to display
-$window->show_all;
+do_foo();
 
 Gtk3->main();
+
+sub do_foo {
+    if ( !$window ) {
+        # Our main window: it holds everything
+        $window = Gtk3::Window->new;
+        $window->set_title('template');
+        $window->signal_connect( destroy => sub { Gtk3->main_quit } );
+
+        # This VBox will be handy to organize objects
+        my $box = Gtk3::Box->new( 'vertical', 5 );
+        $window->add($box);
+    }
+
+    if ( !$window->get_visible ) {
+        # Tell everything to display
+        $window->show_all;
+    } else {
+        $window->destroy;
+    }
+}
+
+    1;
