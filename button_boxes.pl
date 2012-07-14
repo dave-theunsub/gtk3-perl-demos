6efb99d84654b426743171c39cb606f7e132a4ee
Dave M <dave.nerd@gmail.com>
2012-07-14 07:46:49 -0500

    Added license; made to be standalone now

--- button_boxes.pl
+++ button_boxes.pl
@@ -4,85 +4,71 @@
 #
 # The Button Box widgets are used to arrange buttons with padding.
 
-package button_boxes;
-
 use strict;
 use warnings;
 
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-my $window;
 do_button_box();
-
 Gtk3->main();
 
 sub do_button_box {
-    if ( !$window ) {
-        $window = Gtk3::Window->new('toplevel');
-        $window->set_title('Button Boxes');
-        $window->set_border_width(10);
-        $window->signal_connect( destroy => sub { Gtk3->main_quit } );
-        my $icon = 'gtk-logo-rgb.gif';
-        if ( -e $icon ) {
-            my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
-            my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-            $window->set_icon($transparent);
-        }
-
-        my $box = Gtk3::Box->new( 'vertical', 0 );
-        $box->set_homogeneous(FALSE);
-        $window->add($box);
-
-        my $frame_horiz = Gtk3::Frame->new('Horizontal Button Boxes');
-        $box->pack_start( $frame_horiz, TRUE, TRUE, 10 );
-
-        my $vbox = Gtk3::Box->new( 'vertical', 0 );
-        $vbox->set_border_width(10);
-        $vbox->set_homogeneous(FALSE);
-        $frame_horiz->add($vbox);
-
-        $vbox->pack_start( create_bbox( TRUE, 'Spread', 40, 'spread' ),
-            TRUE, TRUE, 0 );
-
-        $vbox->pack_start( create_bbox( TRUE, 'Edge', 40, 'edge' ),
-            TRUE, TRUE, 5 );
-
-        $vbox->pack_start( create_bbox( TRUE, 'Start', 40, 'start' ),
-            TRUE, TRUE, 5 );
-
-        $vbox->pack_start( create_bbox( TRUE, 'End', 40, 'end' ),
-            TRUE, TRUE, 5 );
-
-        my $frame_vert = Gtk3::Frame->new('Vertical Button Boxes');
-        $box->pack_start( $frame_vert, TRUE, TRUE, 10 );
-
-        my $hbox = Gtk3::Box->new( 'horizontal', 0 );
-        $hbox->set_border_width(10);
-        $hbox->set_homogeneous(FALSE);
-        $frame_vert->add($hbox);
-
-        $hbox->pack_start( create_bbox( FALSE, 'Spread', 30, 'spread' ),
-            TRUE, TRUE, 0 );
-
-        $hbox->pack_start( create_bbox( FALSE, 'Edge', 30, 'edge' ),
-            TRUE, TRUE, 5 );
-
-        $hbox->pack_start( create_bbox( FALSE, 'Start', 30, 'start' ),
-            TRUE, TRUE, 5 );
-
-        $hbox->pack_start( create_bbox( FALSE, 'End', 30, 'end' ),
-            TRUE, TRUE, 5 );
-
+    my $window = Gtk3::Window->new('toplevel');
+    $window->set_title('Button Boxes');
+    $window->set_border_width(10);
+    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
+    my $icon = 'gtk-logo-rgb.gif';
+    if ( -e $icon ) {
+        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
+        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+        $window->set_icon($transparent);
     }
 
-    if ( !$window->get_visible ) {
-        $window->show_all();
-    } else {
-        $window->destroy();
-    }
+    my $box = Gtk3::Box->new( 'vertical', 0 );
+    $box->set_homogeneous(FALSE);
+    $window->add($box);
 
-    return $window;
+    my $frame_horiz = Gtk3::Frame->new('Horizontal Button Boxes');
+    $box->pack_start( $frame_horiz, TRUE, TRUE, 10 );
+
+    my $vbox = Gtk3::Box->new( 'vertical', 0 );
+    $vbox->set_border_width(10);
+    $vbox->set_homogeneous(FALSE);
+    $frame_horiz->add($vbox);
+
+    $vbox->pack_start( create_bbox( TRUE, 'Spread', 40, 'spread' ),
+        TRUE, TRUE, 0 );
+
+    $vbox->pack_start( create_bbox( TRUE, 'Edge', 40, 'edge' ),
+        TRUE, TRUE, 5 );
+
+    $vbox->pack_start( create_bbox( TRUE, 'Start', 40, 'start' ),
+        TRUE, TRUE, 5 );
+
+    $vbox->pack_start( create_bbox( TRUE, 'End', 40, 'end' ), TRUE, TRUE, 5 );
+
+    my $frame_vert = Gtk3::Frame->new('Vertical Button Boxes');
+    $box->pack_start( $frame_vert, TRUE, TRUE, 10 );
+
+    my $hbox = Gtk3::Box->new( 'horizontal', 0 );
+    $hbox->set_border_width(10);
+    $hbox->set_homogeneous(FALSE);
+    $frame_vert->add($hbox);
+
+    $hbox->pack_start( create_bbox( FALSE, 'Spread', 30, 'spread' ),
+        TRUE, TRUE, 0 );
+
+    $hbox->pack_start( create_bbox( FALSE, 'Edge', 30, 'edge' ),
+        TRUE, TRUE, 5 );
+
+    $hbox->pack_start( create_bbox( FALSE, 'Start', 30, 'start' ),
+        TRUE, TRUE, 5 );
+
+    $hbox->pack_start( create_bbox( FALSE, 'End', 30, 'end' ), TRUE, TRUE,
+        5 );
+
+    $window->show_all();
 }
 
 sub create_bbox {
@@ -109,4 +95,7 @@ sub create_bbox {
     return $frame;
 }
 
-1;
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
