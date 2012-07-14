531a24d4040558d5478f8c3060430edceb4ff91a
Dave M <dave.nerd@gmail.com>
2012-07-14 08:20:56 -0500

    Added license; made to be standalone now

--- menus.pl
+++ menus.pl
@@ -24,8 +24,7 @@
 # bars and menus; while you can construct menus manually, most
 # people don't do that. There's a separate demo for GtkUIManager.
 #
-
-package menus;
+# Perl version by Dave M <dave.nerd@gmail.com>
 
 use strict;
 use warnings;
@@ -33,86 +32,75 @@ use warnings;
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-my $window;
-
 do_menus();
-
 Gtk3->main();
 
 sub do_menus {
-    if ( !$window ) {
-        $window = Gtk3::Window->new('toplevel');
-        $window->set_title('Menus');
-        $window->set_border_width(0);
-        $window->signal_connect( destroy => sub { Gtk3->main_quit } );
+    my $window = Gtk3::Window->new('toplevel');
+    $window->set_title('Menus');
+    $window->set_border_width(0);
+    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
 
-        my $icon = 'gtk-logo-rgb.gif';
-        if ( -e $icon ) {
-            my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
-            my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-            $window->set_icon($transparent);
-        }
-
-        my $accel_group = Gtk3::AccelGroup->new;
-        $window->add_accel_group($accel_group);
-
-        my $box = Gtk3::Box->new( 'horizontal', 0 );
-        $window->add($box);
-        $box->show();
-
-        my $box1 = Gtk3::Box->new( 'vertical', 0 );
-        $box->add($box1);
-        $box1->show();
-
-        my $menubar = Gtk3::MenuBar->new;
-        $box1->pack_start( $menubar, FALSE, TRUE, 0 );
-        $menubar->show();
-
-        my $menu = create_menu(2);
-
-        my $menuitem = Gtk3::MenuItem->new_with_label("test\nline2");
-        $menuitem->set_submenu($menu);
-        $menubar->append($menuitem);
-        $menuitem->show();
-
-        $menuitem = Gtk3::MenuItem->new_with_label('foo');
-        $menuitem->set_submenu( create_menu(3) );
-        $menubar->append($menuitem);
-        $menuitem->show();
-
-        $menuitem = Gtk3::MenuItem->new_with_label('bar');
-        $menuitem->set_submenu( create_menu(4) );
-        $menubar->append($menuitem);
-        $menuitem->show();
-
-        my $box2 = Gtk3::Box->new( 'vertical', 10 );
-        $box2->set_border_width(10);
-        $box1->pack_start( $box2, FALSE, TRUE, 0 );
-        $box2->show();
-
-        my $flip_button = Gtk3::Button->new_with_label('Flip');
-        $flip_button->signal_connect(
-            clicked => sub {
-                change_orientation($menubar);
-            } );
-        $box2->pack_start( $flip_button, TRUE, TRUE, 0 );
-        $flip_button->show();
-
-        my $close_button = Gtk3::Button->new_with_label('Close');
-        $close_button->signal_connect( clicked => sub { Gtk3->main_quit } );
-        $close_button->set_can_default(TRUE);
-        $close_button->grab_default();
-        $box2->pack_start( $close_button, TRUE, TRUE, 0 );
-        $close_button->show();
+    my $icon = 'gtk-logo-rgb.gif';
+    if ( -e $icon ) {
+        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
+        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+        $window->set_icon($transparent);
     }
 
-    if ( !$window->get_visible ) {
-        $window->show_all();
-    } else {
-        $window->destroy();
-    }
+    my $accel_group = Gtk3::AccelGroup->new;
+    $window->add_accel_group($accel_group);
 
-    #return $window;
+    my $box = Gtk3::Box->new( 'horizontal', 0 );
+    $window->add($box);
+    $box->show();
+
+    my $box1 = Gtk3::Box->new( 'vertical', 0 );
+    $box->add($box1);
+    $box1->show();
+
+    my $menubar = Gtk3::MenuBar->new;
+    $box1->pack_start( $menubar, FALSE, TRUE, 0 );
+    $menubar->show();
+
+    my $menu = create_menu(2);
+
+    my $menuitem = Gtk3::MenuItem->new_with_label("test\nline2");
+    $menuitem->set_submenu($menu);
+    $menubar->append($menuitem);
+    $menuitem->show();
+
+    $menuitem = Gtk3::MenuItem->new_with_label('foo');
+    $menuitem->set_submenu( create_menu(3) );
+    $menubar->append($menuitem);
+    $menuitem->show();
+
+    $menuitem = Gtk3::MenuItem->new_with_label('bar');
+    $menuitem->set_submenu( create_menu(4) );
+    $menubar->append($menuitem);
+    $menuitem->show();
+
+    my $box2 = Gtk3::Box->new( 'vertical', 10 );
+    $box2->set_border_width(10);
+    $box1->pack_start( $box2, FALSE, TRUE, 0 );
+    $box2->show();
+
+    my $flip_button = Gtk3::Button->new_with_label('Flip');
+    $flip_button->signal_connect(
+        clicked => sub {
+            change_orientation($menubar);
+        } );
+    $box2->pack_start( $flip_button, TRUE, TRUE, 0 );
+    $flip_button->show();
+
+    my $close_button = Gtk3::Button->new_with_label('Close');
+    $close_button->signal_connect( clicked => sub { Gtk3->main_quit } );
+    $close_button->set_can_default(TRUE);
+    $close_button->grab_default();
+    $box2->pack_start( $close_button, TRUE, TRUE, 0 );
+    $close_button->show();
+
+    $window->show_all();
 }
 
 sub change_orientation {
@@ -150,4 +138,7 @@ sub create_menu {
     return $menu;
 }
 
-1;
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
