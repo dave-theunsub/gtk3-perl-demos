35cc5e084b47ae61b5a287383c7346d7c16162cd
Dave M <dave.nerd@gmail.com>
2012-07-14 07:59:11 -0500

    Added license; made to be standalone now

--- entry_completion.pl
+++ entry_completion.pl
@@ -4,8 +4,7 @@
 #
 # GtkEntryCompletion provides a mechanism for adding support for completion in GtkEntry.
 #
-
-package entry_completion;
+# Perl version by Dave M <dave.nerd@gmail.com>
 
 use strict;
 use warnings;
@@ -13,9 +12,7 @@ use warnings;
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-my $window;
 do_entry_completion();
-
 Gtk3->main();
 
 sub create_completion_model {
@@ -36,54 +33,49 @@ sub create_completion_model {
 }
 
 sub do_entry_completion {
-    if ( !$window ) {
-        $window = Gtk3::Dialog->new();
-        $window->add_button( 'gtk-close', 1 );
-        $window->set_title('Entry Completion');
+    my $window = Gtk3::Dialog->new();
+    $window->add_button( 'gtk-close', 1 );
+    $window->set_title('Entry Completion');
 
-        $window->set_resizable(FALSE);
-        $window->signal_connect( response => sub { Gtk3->main_quit } );
-        $window->signal_connect( destroy  => sub { Gtk3->main_quit } );
+    $window->set_resizable(FALSE);
+    $window->signal_connect( response => sub { Gtk3->main_quit } );
+    $window->signal_connect( destroy  => sub { Gtk3->main_quit } );
 
-        my $icon = 'gtk-logo-rgb.gif';
-        if ( -e $icon ) {
-            my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
-            my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-            $window->set_icon($transparent);
-        }
-
-        my $box = Gtk3::Box->new( 'vertical', 5 );
-        $box->set_border_width(5);
-        $box->set_homogeneous(FALSE);
-        $window->get_content_area()->add($box);
-
-        my $label = Gtk3::Label->new('');
-        $label->set_markup( 'Completion demo: try writing <b>total</b>'
-                . ' or <b>gnome</b> for example.' );
-        $box->pack_start( $label, FALSE, FALSE, 0 );
-
-        # Create our entry
-        my $entry = Gtk3::Entry->new();
-        $box->pack_start( $entry, FALSE, FALSE, 0 );
-
-        # Create the completion object
-        my $completion = Gtk3::EntryCompletion->new();
-
-        # Assign completion to the entry
-        $entry->set_completion($completion);
-
-        my $completion_model = create_completion_model();
-        $completion->set_model($completion_model);
-        $completion->set_text_column(0);
+    my $icon = 'gtk-logo-rgb.gif';
+    if ( -e $icon ) {
+        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
+        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+        $window->set_icon($transparent);
     }
 
-    if ( !$window->get_visible ) {
-        $window->show_all();
-    } else {
-        $window->destroy();
-    }
+    my $box = Gtk3::Box->new( 'vertical', 5 );
+    $box->set_border_width(5);
+    $box->set_homogeneous(FALSE);
+    $window->get_content_area()->add($box);
 
-    return $window;
+    my $label = Gtk3::Label->new('');
+    $label->set_markup( 'Completion demo: try writing <b>total</b>'
+            . ' or <b>gnome</b> for example.' );
+    $box->pack_start( $label, FALSE, FALSE, 0 );
+
+    # Create our entry
+    my $entry = Gtk3::Entry->new();
+    $box->pack_start( $entry, FALSE, FALSE, 0 );
+
+    # Create the completion object
+    my $completion = Gtk3::EntryCompletion->new();
+
+    # Assign completion to the entry
+    $entry->set_completion($completion);
+
+    my $completion_model = create_completion_model();
+    $completion->set_model($completion_model);
+    $completion->set_text_column(0);
+
+    $window->show_all();
 }
 
-1;
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
