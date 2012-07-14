2a9a43a38a834ac96df7d7c5846023e10db08c46
Dave M <dave.nerd@gmail.com>
2012-07-14 07:53:45 -0500

    Added license; made to be standalone now

--- dialog_boxes.pl
+++ dialog_boxes.pl
@@ -3,15 +3,13 @@
 # Dialog and Message Boxes
 #
 # Dialog widgets are used to pop up a transient window for user feedback.
-
-package dialog_boxes;
+#
 
 use strict;
 use warnings;
+use feature 'state';
 
 use constant GTK_RESPONSE_OK => -5;
-
-use feature 'state';
 
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
@@ -23,72 +21,62 @@ do_dialog();
 Gtk3->main;
 
 sub do_dialog {
-    if ( !$window ) {
-        $window = Gtk3::Window->new('toplevel');
-        $window->set_title('Dialogs');
-        $window->signal_connect( destroy => sub { Gtk3->main_quit } );
-        $window->set_border_width(8);
+    $window = Gtk3::Window->new('toplevel');
+    $window->set_title('Dialogs');
+    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
+    $window->set_border_width(8);
 
-        my $icon = 'gtk-logo-rgb.gif';
-        if ( -e $icon ) {
-            my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
-            my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-            $window->set_icon($transparent);
-        }
-
-        my $frame = Gtk3::Frame->new('Dialogs');
-        $window->add($frame);
-
-        my $vbox = Gtk3::Box->new( 'vertical', 8 );
-        $vbox->set_border_width(8);
-        $frame->add($vbox);
-
-        # Standard message dialog
-        my $hbox = Gtk3::Box->new( 'horizontal', 8 );
-        $vbox->pack_start( $hbox, FALSE, FALSE, 0 );
-        my $button = Gtk3::Button->new_with_mnemonic('_Message Dialog');
-        $button->signal_connect( clicked => \&message_dialog_clicked );
-        $hbox->pack_start( $button, FALSE, FALSE, 0 );
-        $vbox->pack_start( Gtk3::Separator->new('horizontal'),
-            FALSE, FALSE, 0 );
-
-        # Interactive dialog
-        $hbox = Gtk3::Box->new( 'horizontal', 8 );
-        $vbox->pack_start( $hbox, FALSE, FALSE, 0 );
-
-        my $vbox2 = Gtk3::Box->new( 'vertical', 0 );
-        $button = Gtk3::Button->new_with_mnemonic('_Interactive Dialog');
-        $button->signal_connect( clicked => \&interactive_dialog_clicked );
-        $hbox->pack_start( $vbox2, FALSE, FALSE, 0 );
-        $vbox2->pack_start( $button, FALSE, FALSE, 0 );
-
-        my $table = Gtk3::Grid->new();
-        $table->set_row_spacing(4);
-        $table->set_column_spacing(4);
-        $hbox->pack_start( $table, FALSE, FALSE, 0 );
-
-        my $label = Gtk3::Label->new_with_mnemonic('_Entry 1');
-        $table->attach( $label, 0, 0, 1, 1 );
-
-        $entry1 = Gtk3::Entry->new();
-        $table->attach( $entry1, 1, 0, 1, 1 );
-        $label->set_mnemonic_widget($entry1);
-
-        $label = Gtk3::Label->new_with_mnemonic('E_ntry 2');
-        $table->attach( $label, 0, 1, 1, 1 );
-
-        $entry2 = Gtk3::Entry->new();
-        $table->attach( $entry2, 1, 1, 1, 1 );
-
+    my $icon = 'gtk-logo-rgb.gif';
+    if ( -e $icon ) {
+        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file('gtk-logo-rgb.gif');
+        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+        $window->set_icon($transparent);
     }
 
-    if ( !$window->get_visible ) {
-        $window->show_all;
-    } else {
-        $window->destroy();
-    }
+    my $frame = Gtk3::Frame->new('Dialogs');
+    $window->add($frame);
 
-    return $window;
+    my $vbox = Gtk3::Box->new( 'vertical', 8 );
+    $vbox->set_border_width(8);
+    $frame->add($vbox);
+
+    # Standard message dialog
+    my $hbox = Gtk3::Box->new( 'horizontal', 8 );
+    $vbox->pack_start( $hbox, FALSE, FALSE, 0 );
+    my $button = Gtk3::Button->new_with_mnemonic('_Message Dialog');
+    $button->signal_connect( clicked => \&message_dialog_clicked );
+    $hbox->pack_start( $button,                            FALSE, FALSE, 0 );
+    $vbox->pack_start( Gtk3::Separator->new('horizontal'), FALSE, FALSE, 0 );
+
+    # Interactive dialog
+    $hbox = Gtk3::Box->new( 'horizontal', 8 );
+    $vbox->pack_start( $hbox, FALSE, FALSE, 0 );
+
+    my $vbox2 = Gtk3::Box->new( 'vertical', 0 );
+    $button = Gtk3::Button->new_with_mnemonic('_Interactive Dialog');
+    $button->signal_connect( clicked => \&interactive_dialog_clicked );
+    $hbox->pack_start( $vbox2, FALSE, FALSE, 0 );
+    $vbox2->pack_start( $button, FALSE, FALSE, 0 );
+
+    my $table = Gtk3::Grid->new();
+    $table->set_row_spacing(4);
+    $table->set_column_spacing(4);
+    $hbox->pack_start( $table, FALSE, FALSE, 0 );
+
+    my $label = Gtk3::Label->new_with_mnemonic('_Entry 1');
+    $table->attach( $label, 0, 0, 1, 1 );
+
+    $entry1 = Gtk3::Entry->new();
+    $table->attach( $entry1, 1, 0, 1, 1 );
+    $label->set_mnemonic_widget($entry1);
+
+    $label = Gtk3::Label->new_with_mnemonic('E_ntry 2');
+    $table->attach( $label, 0, 1, 1, 1 );
+
+    $entry2 = Gtk3::Entry->new();
+    $table->attach( $entry2, 1, 1, 1, 1 );
+
+    $window->show_all;
 }
 
 sub message_dialog_clicked {
@@ -151,4 +139,7 @@ sub interactive_dialog_clicked {
     $dialog->destroy;
 }
 
-1;
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
