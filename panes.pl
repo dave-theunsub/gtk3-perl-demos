5e1f0653f5fcf3aaf28b602c1fb3c1a26746c0f3
Dave M <dave.nerd@gmail.com>
2012-09-12 08:22:55 -0500

    removed main.pl compatibility

--- panes.pl
+++ panes.pl
@@ -19,68 +19,65 @@ use warnings;
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-do_panes();
-Gtk3->main();
+my $window = Gtk3::Window->new('toplevel');
+$window->signal_connect( destroy => sub { Gtk3->main_quit } );
+$window->set_title('Panes');
+$window->set_border_width(0);
 
-sub do_panes {
-    my $window = Gtk3::Window->new('toplevel');
-    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
-    $window->set_title('Panes');
-    $window->set_border_width(0);
-
-    my $icon = 'gtk-logo-rgb.gif';
-    if ( -e $icon ) {
-        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file($icon);
-        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-        $window->set_icon($transparent);
-    }
-
-    # VBox is deprecated
-    my $vbox = Gtk3::Box->new( 'vertical', 0 );
-    $window->add($vbox);
-
-    # VPaned is deprecated
-    my $vpaned = Gtk3::Paned->new('vertical');
-    $vbox->pack_start( $vpaned, TRUE, TRUE, 0 );
-    $vpaned->set_border_width(5);
-
-    # HPaned is deprecated
-    my $hpaned = Gtk3::Paned->new('horizontal');
-    $vpaned->add1($hpaned);
-
-    my $frame = Gtk3::Frame->new('');
-    $frame->set_shadow_type('in');
-    $frame->set_size_request( 60, 60 );
-    $hpaned->add1($frame);
-
-    my $button = Gtk3::Button->new_with_mnemonic('_Hi there');
-    $frame->add($button);
-
-    $frame = Gtk3::Frame->new('');
-    $frame->set_shadow_type('in');
-    $frame->set_size_request( 80, 60 );
-    #$frame->add( Gtk3::Button->new() );
-    $hpaned->add2($frame);
-
-    $frame = Gtk3::Frame->new('');
-    $frame->set_shadow_type('in');
-    $frame->set_size_request( 60, 80 );
-    #$frame->add( Gtk3::Button->new() );
-    $vpaned->add2($frame);
-
-    # Now create toggle buttons to control sizing
-    $vbox->pack_start(
-        create_pane_options( $hpaned, 'Horizontal', 'Left', 'Right' ),
-        FALSE, FALSE, 0 );
-
-    $vbox->pack_start(
-        create_pane_options( $vpaned, 'Vertical', 'Top', 'Bottom' ),
-        FALSE, FALSE, 0 );
-
-    $vbox->show_all();
-
-    $window->show_all();
+my $icon = 'gtk-logo-rgb.gif';
+if ( -e $icon ) {
+    my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file($icon);
+    my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+    $window->set_icon($transparent);
 }
+
+# VBox is deprecated
+my $vbox = Gtk3::Box->new( 'vertical', 0 );
+$window->add($vbox);
+
+# VPaned is deprecated
+my $vpaned = Gtk3::Paned->new('vertical');
+$vbox->pack_start( $vpaned, TRUE, TRUE, 0 );
+$vpaned->set_border_width(5);
+
+# HPaned is deprecated
+my $hpaned = Gtk3::Paned->new('horizontal');
+$vpaned->add1($hpaned);
+
+my $frame = Gtk3::Frame->new('');
+$frame->set_shadow_type('in');
+$frame->set_size_request( 60, 60 );
+$hpaned->add1($frame);
+
+my $button = Gtk3::Button->new_with_mnemonic('_Hi there');
+$frame->add($button);
+
+$frame = Gtk3::Frame->new('');
+$frame->set_shadow_type('in');
+$frame->set_size_request( 80, 60 );
+#$frame->add( Gtk3::Button->new() );
+$hpaned->add2($frame);
+
+$frame = Gtk3::Frame->new('');
+$frame->set_shadow_type('in');
+$frame->set_size_request( 60, 80 );
+#$frame->add( Gtk3::Button->new() );
+$vpaned->add2($frame);
+
+# Now create toggle buttons to control sizing
+$vbox->pack_start(
+    create_pane_options( $hpaned, 'Horizontal', 'Left', 'Right' ),
+    FALSE, FALSE, 0 );
+
+$vbox->pack_start(
+    create_pane_options( $vpaned, 'Vertical', 'Top', 'Bottom' ),
+    FALSE, FALSE, 0 );
+
+$vbox->show_all();
+
+$window->show_all();
+
+Gtk3->main();
 
 sub create_pane_options {
     my ( $paned, $frame_label, $label1, $label2 ) = @_;
