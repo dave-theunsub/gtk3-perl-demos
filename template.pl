39e131dbaf138a39ea0638eb8845d7133db683b9
Dave M <dave.nerd@gmail.com>
2012-09-12 08:31:03 -0500

    removed main.pl compatibility

--- template.pl
+++ template.pl
@@ -9,19 +9,16 @@ use warnings;
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-do_foo();
+my $window = Gtk3::Window->new;
+$window->set_title('template');
+$window->signal_connect( destroy => sub { Gtk3->main_quit } );
+
+my $box = Gtk3::Box->new( 'vertical', 5 );
+$window->add($box);
+
+$window->show_all;
+
 Gtk3->main();
-
-sub do_foo {
-    my $window = Gtk3::Window->new;
-    $window->set_title('template');
-    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
-
-    my $box = Gtk3::Box->new( 'vertical', 5 );
-    $window->add($box);
-
-    $window->show_all;
-}
 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public License
