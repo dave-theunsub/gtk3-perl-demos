b5d9dafa8a590169114df4db9ca9b6790686d76b
Dave M <dave.nerd@gmail.com>
2012-09-12 08:12:23 -0500

    removed main.pl compatibility

--- expander.pl
+++ expander.pl
@@ -12,50 +12,47 @@ use warnings;
 use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 
-do_expander();
-Gtk3->main();
+my $window = Gtk3::Dialog->new();
+$window->add_button( 'gtk-close', 1 );
+$window->set_title('GtkExpander');
 
-sub do_expander {
-    my $window = Gtk3::Dialog->new();
-    $window->add_button( 'gtk-close', 1 );
-    $window->set_title('GtkExpander');
+$window->set_resizable(FALSE);
+$window->signal_connect( response => sub { Gtk3->main_quit } );
+$window->signal_connect( destroy  => sub { Gtk3->main_quit } );
 
-    $window->set_resizable(FALSE);
-    $window->signal_connect( response => sub { Gtk3->main_quit } );
-    $window->signal_connect( destroy  => sub { Gtk3->main_quit } );
-
-    my $icon = 'gtk-logo-rgb.gif';
-    if ( -e $icon ) {
-        my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file($icon);
-        my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
-        $window->set_icon($transparent);
-    }
-
-    my $box = Gtk3::Box->new( 'vertical', 5 );
-    $box->set_border_width(5);
-    $box->set_homogeneous(FALSE);
-    $window->get_content_area()->add($box);
-
-    my $label = Gtk3::Label->new('');
-    $label->set_markup(
-        'Expander demo.' . ' Click on the triangle for details.' );
-    $box->pack_start( $label, FALSE, FALSE, 0 );
-
-    # Create our expander
-    my $expander = Gtk3::Expander->new('Details');
-    $box->pack_start( $expander, FALSE, FALSE, 0 );
-    $expander->signal_connect(
-        activate => sub {
-            print "Expander has been activated!\n";
-        } );
-
-    # This label will show up once expanded
-    my $label2 = Gtk3::Label->new('');
-    $label2->set_text('Details can be shown or hidden.');
-    $expander->add($label2);
-
-    $window->show_all();
+my $icon = 'gtk-logo-rgb.gif';
+if ( -e $icon ) {
+    my $pixbuf = Gtk3::Gdk::Pixbuf->new_from_file($icon);
+    my $transparent = $pixbuf->add_alpha( TRUE, 0xff, 0xff, 0xff );
+    $window->set_icon($transparent);
 }
+
+my $box = Gtk3::Box->new( 'vertical', 5 );
+$box->set_border_width(5);
+$box->set_homogeneous(FALSE);
+$window->get_content_area()->add($box);
+
+my $label = Gtk3::Label->new('');
+$label->set_markup(
+    'Expander demo.' . ' Click on the triangle for details.' );
+$box->pack_start( $label, FALSE, FALSE, 0 );
+
+# Create our expander
+my $expander = Gtk3::Expander->new('Details');
+$box->pack_start( $expander, FALSE, FALSE, 0 );
+$expander->signal_connect(
+    activate => sub {
+        print "Expander has been activated!\n";
+    } );
+
+# This label will show up once expanded
+my $label2 = Gtk3::Label->new('');
+$label2->set_text('Details can be shown or hidden.');
+$expander->add($label2);
+
+$window->show_all();
+
+Gtk3->main();
 
 # This library is free software; you can redistribute it and/or
 # # modify it under the terms of the GNU Library General Public License
